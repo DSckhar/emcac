50fdb1a58b12fb5e24ce643cf0d0e3294e2f7248
Davi-Mota <davimotta.000@gmail.com>
2019-09-27 20:23:30 +0000

    publicação: agora vai

--- resources/views/site/publicacao/index.blade.php
+++ resources/views/site/publicacao/index.blade.php
@@ -1,140 +1,81 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <title>EMCAC</title>
+@extends('site.site') 
+@section('site')
 
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="{{asset('site/css/publicacao.css')}}">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{asset('site/css/bootstrap.min.css')}}">
-
-    <!-- FontAwesome CSS -->
-    <link rel="stylesheet" href="{{asset('site/css/font-awesome.min.css')}}">
-
-    <!-- ElegantFonts CSS -->
-    <link rel="stylesheet" href="{{asset('site/css/elegant-fonts.css')}}">
-
-    <!-- themify-icons CSS -->
-    <link rel="stylesheet" href="{{asset('site/css/themify-icons.css')}}">
-
-    <!-- Swiper CSS -->
-    <link rel="stylesheet" href="{{asset('site/css/swiper.min.css')}}">
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="{{asset('site/style.css')}}">
-    <link rel="stylesheet" href="{{asset('site/css/publicacao.css')}}">
-
-    </head>
-<body>
-    <div class="">
-        <header>
-            <div class="nav-bar" style="background-color: #56555B;">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-9 col-lg-3">
-                            <div class="site-branding">
-                                <h1 class="site-title"><a href="{{asset('/')}}" rel="home">EM<span>CAC</span></a></h1>
-                            </div><!-- .site-branding -->
-                        </div><!-- .col -->
-
-                        <div class="col-3 col-lg-9 flex justify-content-end align-content-center">
-                            <nav class="site-navigation flex justify-content-end align-items-center">
-                                <ul class="flex flex-column flex-lg-row justify-content-lg-end align-content-center">
-                                    <li class="current-menu-item" disabled><a href="">Home</a></li>
-                                    <li><a href="{{asset('/')}}">Home</a></li>
-                                    <li><a href="#">Publicações</a></li>
-                                    <li><a href="#">Documentos</a></li>
-                                    <li><a href="#">informação</a></li>
-                                    <li><a href="#">Sobre</a></li>
-                                </ul>
-                                
-
-                                <div class="hamburger-menu d-lg-none">
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                </div><!-- .hamburger-menu -->
-                            </nav><!-- .site-navigation -->
-                        </div><!-- .col -->
-                    </div><!-- .row -->
-                </div><!-- .container -->
-            </div><!-- .nav-bar -->
-        </header><!-- .site-header -->
-
-        <main class="container-fluid" id="cont">
-            <div class="row">
-                <div class="sidebar text-center" >
-                    <img src="" class="img-circle">
-                    <h3>User Name</h3>
-                    Publicação 1<br/>
-                    Publicação 2<br/>
-                    Publicação 3<br/>
-                    Publicação 4<br/>
-                    Publicação 5<br/>
-                    Publicação 6<br/>
-                    Publicação 7<br/>
-                    Publicação 8<br/>
-                    Publicação 9<br/>
-                    Publicação 10<br/>
-                    Publicação 11<br/>
-                </div>
-                <div class="content">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col">
-                                <div class="card" style="width: 65rem;">
-                                    <div class="card-body">
-                                        <h1 class="card-title">Publicação 1</h1>
-                                        <figure id="figura">
-                                            <img src="https://pt.seaicons.com/wp-content/uploads/2015/06/news-icon1.png" alt="">
-                                        </figure>
-                                        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.
-                                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint iusto laborum dolorem enim cumque minus, alias exercitationem repudiandae
-                                             repellat nulla sequi quam ducimus maxime saepe eaque consequatur illum voluptas officia.
-                                        </p>
-                                    </div>
-                                </div>
+<main class="container-fluid" id="cont">
+    <div class="row">
+        <div class="sidebar text-center" >
+            <img src="" class="img-circle">
+            <h3>User Name</h3>
+            Publicação 1<br/>
+            Publicação 2<br/>
+            Publicação 3<br/>
+            Publicação 4<br/>
+            Publicação 5<br/>
+            Publicação 6<br/>
+            Publicação 7<br/>
+            Publicação 8<br/>
+            Publicação 9<br/>
+            Publicação 10<br/>
+            Publicação 11<br/>
+        </div>
+        <div class="content">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="card" style="width: 65rem;">
+                            <div class="card-body">
+                                <h1 class="card-title">Publicação 1</h1>
+                                <figure id="figura">
+                                    <img src="https://pt.seaicons.com/wp-content/uploads/2015/06/news-icon1.png" alt="">
+                                </figure>
+                                <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint iusto laborum dolorem enim cumque minus, alias exercitationem repudiandae
+                                    repellat nulla sequi quam ducimus maxime saepe eaque consequatur illum voluptas officia.
+                                    <button type="button" class="btn btn-light">Mais...</button>
+                                </p>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col">
-                                <div class="card" style="width: 65rem;">
-                                    <div class="card-body">
-                                        <h1 class="card-title">Publicação 2</h1>
-                                        <figure id="figura">
-                                            <img src="https://pt.seaicons.com/wp-content/uploads/2015/06/news-icon1.png" alt="">
-                                        </figure>
-                                        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.
-                                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint iusto laborum dolorem enim cumque minus, alias exercitationem repudiandae
-                                             repellat nulla sequi quam ducimus maxime saepe eaque consequatur illum voluptas officia.
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div> 
-                        <div class="row">
-                            <div class="col">
-                                <div class="card" style="width: 65rem;">
-                                    <div class="card-body">
-                                        <h1 class="card-title">Publicação 3</h1>
-                                        <figure id="figura">
-                                            <img src="https://pt.seaicons.com/wp-content/uploads/2015/06/news-icon1.png" alt="">
-                                        </figure>
-                                        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.
-                                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint iusto laborum dolorem enim cumque minus, alias exercitationem repudiandae
-                                             repellat nulla sequi quam ducimus maxime saepe eaque consequatur illum voluptas officia.
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>   
                     </div>
                 </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="card" style="width: 65rem;">
+                            <div class="card-body">
+                                <h1 class="card-title">Publicação 2</h1>
+                                <figure id="figura">
+                                    <img src="https://pt.seaicons.com/wp-content/uploads/2015/06/news-icon1.png" alt="">
+                                </figure>
+                                <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint iusto laborum dolorem enim cumque minus, alias exercitationem repudiandae
+                                        repellat nulla sequi quam ducimus maxime saepe eaque consequatur illum voluptas officia.
+                                        <button type="button" class="btn btn-light">Mais...</button>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+                <div class="row">
+                    <div class="col">
+                        <div class="card" style="width: 65rem;">
+                            <div class="card-body">
+                                <h1 class="card-title">Publicação 3</h1>
+                                <figure id="figura">
+                                    <img src="https://pt.seaicons.com/wp-content/uploads/2015/06/news-icon1.png" alt="">
+                                </figure>
+                                <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint iusto laborum dolorem enim cumque minus, alias exercitationem repudiandae
+                                        repellat nulla sequi quam ducimus maxime saepe eaque consequatur illum voluptas officia.
+                                        <button type="button" class="btn btn-light">Mais...</button>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>   
             </div>
-        </main>
+        </div>
     </div>
-</body>
+</main>
+
+@endsection
