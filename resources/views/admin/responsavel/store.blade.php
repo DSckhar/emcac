a85801e1eef074a47536f0f79fe7da31f0cc7431
ADSRP <rick23_gbi@hotmail.com>
2019-10-01 10:47:24 -0300

    formulario responsavel, validação de CPF

--- resources/views/admin/responsavel/store.blade.php
+++ resources/views/admin/responsavel/store.blade.php
@@ -1,40 +1,58 @@
 @extends('admin.admin') 
 @section('adm')
     <script>
-        function VerificaCPF(cpf){
-            var cpf;
-            cpf = vercpf(document.frmcpf.cpf.value);
+        function validarCPF(){
+            var cpf = validar.cpf.value;
+
+            function mensagemCPF(){
+                var erro = "Digite um CPF valido"
+                var span = document.getElementById("CpfInvalido");
+
+                span.innerText = erro;
+
+                alert("CPF invalido!");
+                $("#cpf").focus().css("borderColor", "red").css("color", "red");
+                
+            }
+
             var numeros, digitos, soma, i, resultado, digitos_iguais;
             digitos_iguais = 1;
-            if (cpf.length < 11)
-                    return false;
-            for (i = 0; i < cpf.length - 1; i++)
-                    if (cpf.charAt(i) != cpf.charAt(i + 1))
-                        {
-                        digitos_iguais = 0;
-                        break;
-                        }
-            if (!digitos_iguais)
-                    {
-                    numeros = cpf.substring(0,9);
-                    digitos = cpf.substring(9);
-                    soma = 0;
-                    for (i = 10; i > 1; i--)
-                        soma += numeros.charAt(10 - i) * i;
+            if (cpf.length < 11){
+                mensagemCPF();
+                return false;
+                
+            }
+            for (i = 0; i < cpf.length - 1; i++){
+                if (cpf.charAt(i) != cpf.charAt(i + 1)){
+                    digitos_iguais = 0;
+                    break;
+                }
+            }
+            if (!digitos_iguais){
+                numeros = cpf.substring(0,9);
+                digitos = cpf.substring(9);
+                soma = 0;
+                for (i = 10; i > 1; i--)
+                    soma += numeros.charAt(10 - i) * i;
                     resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-                    if (resultado != digitos.charAt(0))
+                    if (resultado != digitos.charAt(0)){
+                        mensagemCPF();
                         return false;
+                    }
                     numeros = cpf.substring(0,10);
                     soma = 0;
                     for (i = 11; i > 1; i--)
                         soma += numeros.charAt(11 - i) * i;
-                    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-                    if (resultado != digitos.charAt(1))
+                        resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+                    if (resultado != digitos.charAt(1)){
+                        mensagemCPF();
                         return false;
-                    return true;
                     }
-            else
-                    return false;
+                    return true;
+            }else{
+                mensagemCPF();
+                return false;
+            }
         }
     </script>
     
@@ -51,7 +69,7 @@
             </div>
             <div class="block-content">
                 <div class="contanier-fluid">
-                    <form name="frmcpf" method='post' action="/aluno/cadastrado" onsubmit="VerificaCPF();">
+                    <form name="validar" method='post' action="/aluno/cadastrado">
                         <div class="row justify-content-center">
                             <div class="col-6">
                                 {!! csrf_field() !!}
@@ -62,14 +80,24 @@
 
                                 <label for="cpf">CPF</label>
                                 <div class="form-group">
-                                    <input id="cpf" class="form-control" type="text" name="cpf" pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" title="Digite um CPF no formato: xxx.xxx.xxx-xx" maxlength="14"  required/>
+                                    <input id="cpf" class="form-control" type="text" name="cpf" maxlength="14" placeholder="Digita o CPF sem pontuação"  required/>
+                                    <span id="CpfInvalido"></span>
                                 </div>
-                    
+
+                                <label for="telefone">Telefone</label>
+                                <div class="form-group">
+                                    <input id="telefone" class="form-control" type="tel" name="telefone" maxlength="20" required/>
+                                </div>
+
+                                <label for="email">Email</label>
+                                <div class="form-group">
+                                    <input id="email" class="form-control" type="email" name="email" maxlength="40" required/>
+                                </div>
                             </div>
                         </div>
                         <div class="row justify-content-center">
                             <div class="col-2">
-                                <button type="submit" onclick="VerificaCPF();" class="btn btn-primary">Enviar</button>
+                                <button type="submit" onclick="return validarCPF();" class="btn btn-primary">Enviar</button>
                             </div>
                         </div>
                         <br/>
