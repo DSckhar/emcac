319c00e1e29cb6a9fc10fe399772df3a64c9a9b1
DSckhar <teixeira.douglassilva01@gmail.com>
2019-10-17 14:30:30 -0300

    ajuste de layout de publicação(site)

--- resources/views/site/publicacao/index.blade.php
+++ resources/views/site/publicacao/index.blade.php
@@ -1,12 +1,5 @@
 @extends('site.site') 
 @section('site')
-
-<script>
-    // var idTipoPublic= {{$idTipoPublicacao}};
-    
-    // $('#'+idTipoPublic).classList.add("active");
-</script>
-
 <div class="container mt-3 mb-3">
     <h1>Publicações</h1>
 
@@ -15,7 +8,11 @@
 
             <div id="livro" class="list-group">
                 @foreach($tipos as $tipo)
-                    <a id="{{$tipo->id}}" class="list-group-item list-group-item-action" href="/publicacoes/{{$tipo->id}}">{{$tipo->nome}}</a>
+                    @if($idTipoPublicacao == $tipo->id)
+                        <a id="{{$tipo->id}}" class="list-group-item list-group-item-action active" style="background-color: #19c880; border-color: #19c880;" href="/publicacoes/{{$tipo->id}}">{{$tipo->nome}}</a>
+                    @else
+                        <a id="{{$tipo->id}}" class="list-group-item list-group-item-action" href="/publicacoes/{{$tipo->id}}">{{$tipo->nome}}</a>
+                    @endif
                 @endforeach
 
                 <a class="list-group-item list-group-item-action" href="/publicacoes">Limpar filtro</a>
@@ -26,33 +23,30 @@
             <div class="content">
                 <div class="container">
                     @foreach($publicacoes as $publicacao)
-                        <div class="card mb-3" style="background-color: #56555B">
-                            <div id="espaco" class="row">
+                        <div id="espaco" class="card mb-3" style="background-color: #56555B">
+                            <div  class="row">
+                                <div class="col-12 col-md-6">
                                     @if($publicacao->foto != '')
-                                        <!-- <img class="card-img-top" src='{{asset("storage/media/fotos/{$publicacao->foto}")}}' style="width: 100%;" alt=""> -->
-                                        <div class="col-6" id="img" style="background-image: url('{{asset("storage/media/fotos/{$publicacao->foto}")}}');">
-                                            
-                                        </div>
+                                        <img class="card-img-top" height="100%" src='{{asset("storage/media/fotos/{$publicacao->foto}")}}' style="width: 100%;" alt="">
+                                    
                                     @elseif($publicacao->foto == '')
-                                    <div class="col-6" id="img" style="background-image: url('{{asset("site/images/semFoto.png")}}');">
-                                            
-                                        </div>
-                                    @endif
-                                <div class="card-body col-6 text-white">
+                                    <img class="card-img-top"  height="100%" src='{{asset("site/images/semFoto.png")}}' style="width: 100%;" alt="">
+                                    @endif                                
+                                </div>
+                                <div class="card-body col-12 col-md-6 text-white">
                                     <div class="row">
                                         <div class="col">
                                             <h4 class="card-title" style="text-transform: uppercase">{{$publicacao->titulo}}</h4>
-                                            <span class="badge badge-pill badge-info">{{$publicacao->nome}}</span>
-                                            <p class="card-text">Data de Início: <br/>{{$publicacao->dInicio}}</p>
-                                            <p class="card-text">Data de Término: <br/>{{$publicacao->dTermino}}</p>
+                                            <span class="badge badge-pill" style="background-color: #19c880; border-color: #19c880;">{{$publicacao->nome}}</span>
+                                            <p class="card-text">Data de Início: <br/>{{date('d/m/Y', strtotime($publicacao->dInicio))}}</p>
+                                            <p class="card-text">Data de Término: <br/>{{date('d/m/Y', strtotime($publicacao->dTermino))}}</p>
                                         </div>
                                     </div>
                                     <div id="more" class="row justify-content-end">
-                                        <div class="col-4">
-                                            <button class="btn btn-info" onclick="window.location.href='/publicacao/{{$publicacao->id}}'" >Mais...</button>                          
+                                        <div class="col-4 col-md-6 col-lg-4">
+                                            <button class="btn btn-success" style="background-color: #19c880; border-color: #19c880;" onclick="window.location.href='/publicacao/{{$publicacao->id}}'" >Mais...</button>                          
                                         </div>
                                     </div>
-
                                 </div>
                             </div>
                         </div>
